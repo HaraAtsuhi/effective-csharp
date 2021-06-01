1d6da4a7b5bc389538c7c440ff2968bbbeac4c65
Harada Atsushi(原田 篤志) <8210168@phchd.com>
2021-06-01 19:08:45 +0900

    first commit

---
+++ ch13-example/呼び出し順確認/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace 呼び出し順確認
+{
+    class Program
+    {
+        class CallOrder
+        {
+            public CallOrder()
+            {
+                Console.WriteLine("CallOrderのコンストラクタ");
+            }
+            static CallOrder()
+            {
+                Console.WriteLine("CallOrderのstaticコンストラクタ");
+            }
+
+            public CallOrder2 a = new CallOrder2();
+
+            static public void Print()
+            {
+                Console.WriteLine("CallOrderのPrintメソッド");
+            }
+        }
+        public class CallOrder2
+        {
+            public CallOrder2()
+            {
+                Console.WriteLine("フィールドの初期化");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mainメソッド開始");
+            CallOrder callOdrer = new CallOrder();
+            CallOrder.Print();
+            Console.WriteLine("Mainメソッド終了");
+            Console.ReadLine();
+        }
+    }
+}
